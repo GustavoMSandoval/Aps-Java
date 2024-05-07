b10159e28b7eda74dfbe3e8b61505341f95d12d6
GustavoMSandoval <148093617+GustavoMSandoval@users.noreply.github.com>
2024-05-07 14:48:27 -0300

    Add files via upload

--- PrincipalAPS/src/view/view_ConsumoSetor.java
+++ PrincipalAPS/src/view/view_ConsumoSetor.java
@@ -18,6 +18,7 @@ import org.apache.poi.ss.usermodel.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import model.*;
+import model.ExcelTratamento.LeitorExcel;
         
 
 
@@ -193,52 +194,11 @@ public class view_ConsumoSetor extends javax.swing.JFrame {
 
         if (userSelection == JFileChooser.APPROVE_OPTION) {
             File file = fileChooser.getSelectedFile();
-            lerExcel(file);
+            LeitorExcel leitor = new LeitorExcel();
+            leitor.lerExcel(file);
         }
     }//GEN-LAST:event_importarArquivoActionPerformed
-private void lerExcel(File file) {
-         ConsumoSetor consumo = new ConsumoSetor();  
-        try {
-            FileInputStream fileInputStream = new FileInputStream(file);
-            Workbook workbook = WorkbookFactory.create(fileInputStream);
-            Sheet sheet = workbook.getSheetAt(0);
 
-            StringBuilder data = new StringBuilder();
-
-              for (int i = 1; i < 10; i++) {
-                int Contador = 0;
-                Row row = sheet.getRow(i);
-                if (row != null) {
-                    
-                    Cell cell = row.getCell(Contador);
-                    Contador++;
-                    String valorCell = cell != null ? cell.getStringCellValue() : ""; // Obtém o valor da célula como texto
-                    System.out.println(valorCell);
-                    Cell cell1 = row.getCell(Contador);
-                    Contador++;
-                    
-                    Date dataInfo = null;
-                    if (cell1 != null) {
-                        dataInfo = cell1.getDateCellValue(); // Obtém o valor da célula como data
-                    }                 
-                    System.out.println(dataInfo);
-                    
-                    Cell cell2 = row.getCell(Contador);
-                    float valorFloat = cell2 != null ? (float) cell2.getNumericCellValue() : 0.0f; // Obtém o valor da célula como float
-                    Contador++;
-                    System.out.println(valorFloat);        
-                    consumo.setIndicador(valorCell, dataInfo, valorFloat);
-                  }    
-              }
-          
-            workbook.close();
-            
-        } catch (IOException | InvalidFormatException e) {
-            e.printStackTrace();
-            JOptionPane.showMessageDialog(this, "Erro ao ler o arquivo Excel: " + e.getMessage(),
-                    "Erro", JOptionPane.ERROR_MESSAGE);
-        }
-    }
     private void btnAbrirMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnAbrirMouseClicked
         if(this.getExtendedState()!= Home.MAXIMIZED_BOTH){this.setExtendedState(Home.MAXIMIZED_BOTH);}
         else{this.setExtendedState(Home.NORMAL);}
